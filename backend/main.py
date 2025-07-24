22cb6e306eea00f6dac7caa6d5e0201158aac69f
VaishakhVipin <vaishakh.vipin@gmail.com>
2025-07-24 16:27:49 +0530

    updated main fastapi app

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from routes.voice import router as voice_router
 
 app = FastAPI()
-app.include_router(voice_router, prefix="/voice")
+app.include_router(voice_router)
 
 @app.get("/")
 def home():
